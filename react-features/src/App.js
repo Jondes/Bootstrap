58eec0b7dcfcfd451a7248ec723a4b78c4a06541
Johnson E Guedes <8356741+Jondes@users.noreply.github.com>
2022-10-11 06:56:29 +0100

    Initial commit

---
+++ react-features/src/App.js
@@ -0,0 +1,10 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Home from './components/Home'
+export default function App() {
+  return (
+    <div className="container mt-5">
+      <Home />
+    </div>
+  )
+}
